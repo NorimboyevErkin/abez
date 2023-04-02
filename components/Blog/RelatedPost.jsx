98269409f815811a0403bc5ee98ebb518029b22b
NorimboyevErkin <norimboyev19@gmail.com>
2023-04-02 04:56:12 -0700

    wqe

--- components/Blog/RelatedPost.jsx
+++ components/Blog/RelatedPost.jsx
@@ -6,7 +6,7 @@ const RelatedPost = ({ image, slug, title, date }) => {
 		<div className="flex items-center lg:block xl:flex">
 			<div className="mr-5 lg:mb-3 xl:mb-0">
 				<div className="relative h-[60px] w-[70px] overflow-hidden rounded-md sm:h-[75px] sm:w-[85px]">
-					<Image src={image} alt={title} fill />
+				<Image src={image} alt={title} fill />
 				</div>
 			</div>
 			<div className="w-full">
