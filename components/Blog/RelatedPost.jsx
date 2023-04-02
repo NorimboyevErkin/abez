5a546caa030850b1a6a126802b3a5647e293390d
NorimboyevErkin <norimboyev19@gmail.com>
2023-04-02 05:47:33 -0700

    w

--- components/Blog/RelatedPost.jsx
+++ components/Blog/RelatedPost.jsx
@@ -8,15 +8,15 @@ const RelatedPost = ({ image, slug, title, date }) => {
 				<div className="relative h-[60px] w-[70px] overflow-hidden rounded-md sm:h-[75px] sm:w-[85px]">
 				<Image src={image} alt={title} fill />
 				</div>
-			  </div>
-			  <div className="w-full">
+			</div>
+			<div className="w-full">
 				<h5>
 				<Link
 				href={slug}
 				className="mb-[6px] block text-base font-medium leading-snug text-black hover:text-primary dark:text-white dark:hover:text-primary"
 				>
-				{title}
-				</Link>
+						{title}
+					</Link>
 				</h5>
 				<p className="text-xs font-medium text-body-color">{date}</p>
 			</div>
